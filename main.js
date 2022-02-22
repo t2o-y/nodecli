a32f350b93bf55b18c8ee9ba2d3803ff37bb9e88
t2o-y <73864551+t2o-y@users.noreply.github.com>
2022-02-23 06:21:19 +0900

    ファイルを読み込んでコンソールに表示する

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+// commanderモジュールをprogramオブジェクトとしてインポートする
+const program = require("commander");
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
+// コマンドライン引数からファイルパスを取得する
+program.parse(process.argv);
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+
+// ファイルパスをUTF-8として非同期で読み込む
+fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        // 終了ステータス１（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
+    console.log(file);
+});
