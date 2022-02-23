f79d746d14d98278105cf6113173e4186c1e44ed
t2o-y <73864551+t2o-y@users.noreply.github.com>
2022-02-23 10:19:01 +0900

    MarkdownをHTMLに変換する

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodecli
+
 Node.jsでCLIアプリケーション
+https://t2o-y.github.io/nodecli/README.md
--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 const program = require("commander");
 // fsモジュールをfsオブジェクトとしてインポートする
 const fs = require("fs");
+// markedモジュールをmarkedオブジェクトとしてインポートする
+const md = require("marked");
 
+// gfm(Git Flavored Markdown)オプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+}
 
 // ファイルパスをUTF-8として非同期で読み込む
 fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
@@ -16,5 +26,10 @@ fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
         process.exit(1);
         return;
     }
-    console.log(file);
+    // MarkdownファイルをHTML文字列に変換する
+    const html = md.marked(file,{
+        // オプションの値を使用する
+        gfm: cliOptions.gfm,
+    });
+    console.log(html);
 });
